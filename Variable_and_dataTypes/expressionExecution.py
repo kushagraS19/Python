ffe1574f0ef4987a73e0c75127cfe7c30438f468
Anish Saini <anishsaini9098@gmail.com>
2025-11-01 12:55:24 +0530

    learned expression execution

---
+++ Variable_and_dataTypes/expressionExecution.py
@@ -0,0 +1,52 @@
+# String & numeric values can operate together with * operator.
+
+a,b = 2,4
+txt = "k"
+
+print(b*txt)
+
+# String & string can operate together with + operator.
+
+s1 = "ku"
+s2 = "sh"
+
+print((s1 + s2) + "agra")
+
+# Numeric values can operate with all arithmetic operators.
+
+a,b = 5,7
+c = 5
+
+print(" ")
+print(a + b * c)
+
+# Arithmetic expression with integer and float values will result in Float.
+
+n,k = 3,4.9
+
+print("")
+print(n*k)
+
+# Result of division operator with two integers will be float.
+
+num1 = 1
+num2 = 2
+
+print("")
+print(num1/num2)
+
+# INTEGER DIVISION with float and int will give result as int but will display as float.
+
+num3 = 15 
+num4 = 3
+Id = num3//num4
+
+print("")
+print(Id, num3/num4)
+
+# floor gives closest integer, which is lesser or equal to the float value.
+# Result of A//B is same as floor(A/B)
+
+# Remainder is negative only when the denominator is negative.
+
+"""huhuhe"""
