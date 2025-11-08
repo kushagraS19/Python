605fb899952e09acfb8c407cb7ae9982c57fe459
Anish Saini <anishsaini9098@gmail.com>
2025-11-08 18:15:02 +0530

    practicing

--- Practice_sets/chapter_2.py
+++ Practice_sets/chapter_2.py
@@ -7,14 +7,55 @@ print("Length :",len(name))"""
 
 # Write a program to find the occurence of $ in a string.
 
-str = "I $am $kushagra $saini."
-print(str.count("$"))
+# str = "I $am $kushagra $saini."
+# print(str.count("$"))
 
-#_______________________________________________________________________________________________________________________
+# #_______________________________________________________________________________________________________________________
 
-str1 = "I am kushagra saini"
-print(str1[5:len(str1)])
+# str1 = "I am kushagra saini"
+# print(str1[5:len(str1)])
 
 #_____________________________________________________________________________________________________________
 
-print(str1[::-1])
+# print(str1[::-1]) # Reversed string
+
+#___________________________________________________________________________________________________________________
+
+                             # Conditional Statements
+
+
+# WAP to check if a number is even or odd.
+
+"""num = int(input("Enter a number : "))
+
+if(num % 2 == 0):
+    print("Even")
+else:
+    print("Odd")"""    
+
+ #__________________________________________________________________________________________________________________
+
+
+# WAP to find greatest of 3 numbers.  
+
+"""num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+num3 = int(input("Enter third number : "))
+
+if(num1 > num2 and num1 > num3):
+    print(num1, "is greatest value.")
+elif(num2 > num1 and num2 > num3):
+    print(num2, "is greatest value.")
+else:
+    print(num3, "is greatest value")"""
+
+#_____________________________________________________________________________________________________________________-
+
+# WAP to check if a number is multiple of 7.
+
+num = int(input("Enter a number : "))
+
+if(num % 7 == 0):
+    print(num,"is a multiple of 7.")
+else:
+    print(num,"is not a multiple of 7.")
