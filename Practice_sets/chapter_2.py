8d93aefa9c41cf137b9318ab3dcc8878f3f00ede
Anish Saini <anishsaini9098@gmail.com>
2025-11-08 17:47:03 +0530

    practicing

---
+++ Practice_sets/chapter_2.py
@@ -0,0 +1,20 @@
+# Write a program to input user's first name & print its length.
+
+"""name = input("Enter your first name : ")
+print("Length :",len(name))"""
+
+#____________________________________________________________________________________________________________________________
+
+# Write a program to find the occurence of $ in a string.
+
+str = "I $am $kushagra $saini."
+print(str.count("$"))
+
+#_______________________________________________________________________________________________________________________
+
+str1 = "I am kushagra saini"
+print(str1[5:len(str1)])
+
+#_____________________________________________________________________________________________________________
+
+print(str1[::-1])
