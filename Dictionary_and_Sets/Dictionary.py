29352ac219b543fbaca962d11797a77c31a1e649
AnishS109 <anishsaini9098@gmail.com>
2025-11-14 00:45:43 +0530

    Learning dictionaries

---
+++ Dictionary_and_Sets/Dictionary.py
@@ -0,0 +1,41 @@
+# Dictionaries are used to store data values in key:value pairs.
+# They are unordered, mutable and don't allow duplicate keys.
+# We can also store lists and tuples in dictionary.
+
+"""info = {
+  "name" : "Hehe",
+  "age" : 19,
+  "phoneNumber" : 9202216169,
+  "is_adult" : True,
+  "subjects" : ["C++" , "Java" , "Pyhton"],
+  "Marks" : 87.6,
+  12 : 45,
+  34.66 : 67,
+  () : 56
+}
+
+info["surname"] = "Saini" # We can add keys and values in a dictionary.
+info["name"] = "Kushagra" # We can also overwrite exixting values.
+
+print("My name is :",info["name"],info["surname"])
+print("I got",info["Marks"],"marks in",info["subjects"])"""
+
+#_____________________________________________________________________________________________________
+
+# Nested Dictionaries -->
+
+"""student = {
+  "name" : "Kushagra",
+  "marks" : {
+    "maths" : 60,
+    "Physics" : 64,
+    "Chemistry" : 74, 
+  }
+}
+
+print(student["marks"]["maths"])"""
+
+#__________________________________________________________________________________________________
+
+# Methods on Dictionaries -->
+
