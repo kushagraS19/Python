7e37bb80970af727d36cb45ff89d165b553848f0
Kushagra Saini <kushagra.saini19@gmail.com>
2025-11-15 17:58:03 +0530

    chapter 5 completed

---
+++ Practice_sets/chapter5.py
@@ -0,0 +1,16 @@
+# Print numbers from 1 to 100 using for and range-->
+
+"""for i in range(1,101):
+    print(i)"""
+
+# Print nums from 100 to 1 -->
+
+"""for i in range(100,0,-1):
+    print(i)"""
+
+# Print table of n --> 
+
+n = int(input("Enter a number : "))
+
+for i in range(1,11):
+    print(i*n)
